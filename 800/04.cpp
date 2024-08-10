7de3bd87dd30724d077108c8f2159bb4622b82af
NinjaOP241 <ninjadev241@gmail.com>
2024-08-10 09:32:34 +0530

    Game with Integers

---
+++ 800/04.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+
+        if(n % 3 == 0) {
+            cout<<"Second"<<'\n';
+        } else {
+            cout<<"First"<<'\n';
+        }
+
+    }
+    return 0;
+}
